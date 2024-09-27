bf8fd73cc515b928163ed798f50d6129ad30adbf
BigDaveCoding <bigdavecoding@gmail.com>
2024-09-27 15:08:27 +0100

    overlay_hist()
    I can use this function to plot a histogram for one column or multiple columns so my previous function of plot_histogram is null

--- ileostomy_frequency/analyse_ileostomy_data.py
+++ ileostomy_frequency/analyse_ileostomy_data.py
@@ -26,31 +26,31 @@ print(df['amount_percent'].value_counts())
 
 print(df.head(1))
 
-def plot_histogram(column_name):
-    # Testing a histogram
-    unique_vals = df[column_name].unique()
-    bins = np.arange(min(unique_vals) - 0.5, max(unique_vals) + 1.5, 1)
-    df[column_name].hist(bins=bins, color='green', edgecolor='black', rwidth=0.8)
-    plt.title(f'Histogram of {column_name}')
-    plt.xlabel(column_name)
-    plt.ylabel('Frequency')
+# def plot_histogram(column_name):
+#     # Testing a histogram
+#     unique_vals = df[column_name].unique()
+#     bins = np.arange(min(unique_vals) - 0.5, max(unique_vals) + 1.5, 1)
+#     df[column_name].hist(bins=bins, color='green', edgecolor='black', rwidth=0.8)
+#     plt.title(f'Histogram of {column_name}')
+#     plt.xlabel(column_name)
+#     plt.ylabel('Frequency')
 
-    max_x = df[column_name].max()
+#     max_x = df[column_name].max()
 
-    match max_x:
-        case x if 0 <= x <= 10:
-            plt.xticks(np.arange(0, max_x + 1, 1))
-        case x if 10 < x <= 100:
-            plt.xticks(np.arange(0, max_x + 5, 5))
-        case x if x > 100:
-            print('Error')
-        case _:
-            print('unknown case')
+#     match max_x:
+#         case x if 0 <= x <= 10:
+#             plt.xticks(np.arange(0, max_x + 1, 1))
+#         case x if 10 < x <= 100:
+#             plt.xticks(np.arange(0, max_x + 5, 5))
+#         case x if x > 100:
+#             print('Error')
+#         case _:
+#             print('unknown case')
     
-    # plt.xticks(np.arange(0, max_x + 5, 5))
-    max_y = df[column_name].value_counts().max()
-    plt.yticks(np.arange(0, max_y + 1, 1))
-    plt.show()
+#     # plt.xticks(np.arange(0, max_x + 5, 5))
+#     max_y = df[column_name].value_counts().max()
+#     plt.yticks(np.arange(0, max_y + 1, 1))
+#     plt.show()
 
 # print(plot_histogram('amount_percent'))
 # print(plot_histogram('consistency'))
@@ -94,7 +94,8 @@ def overlay_hist(column_list):
     plt.show()
 
 # Example usage
-overlay_hist(['amount_percent', 'consistency'])
+print(overlay_hist(['amount_percent', 'consistency']))
+print(overlay_hist(['consistency']))
 
 # df['consistency'].plot(kind='density', label='consistency')
 # df['amount_percent'].plot(kind='density', label='amount')
